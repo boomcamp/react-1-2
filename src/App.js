cfa2c5de4db7242e99ba30f6cdd9a71dcdcfc587
marvin banton <marvin.banton@boom.camp>
2019-06-21 17:11:39 +0800

    Done Step 4, Part 1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import React, { Component } from 'react';
 import Product from './components/Product';
+import CartItem from './components/CartItem';
 import './App.css';
 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      cart: [],
-      hats: [
-        {
-          id: 1,
-          title: "Fisherman's Hat",
-          description:
-            'Headgear commonly used by fishermen. Increases fishing skill marginally.',
-          price: 12.99,
-          imageUrl: 'https://via.placeholder.com/150x150',
-        },
-        {
-          id: 2,
-          title: 'Metal Hat',
-          description: 'Uncomfortable, but sturdy.',
-          price: 8.99,
-          imageUrl: 'https://via.placeholder.com/150x150',
-        },
-      ],
-      beachGear: [
-        {
-          id: 3,
-          title: 'Tent',
-          description: 'Portable shelter.',
-          price: 32.99,
-          imageUrl: 'https://via.placeholder.com/150x150',
-        },
-      ],
-    };
+           cart: [],
+           cardView: true,
+           address: '',
+           creditCard: '',
+           Motorcycles: [
+               {
+                   id: 1,
+                   imageUrl: 'https://media.karousell.com/media/photos/products/2018/12/21/2019_yamaha_sniper_available_1545379408_f1ae7b760',
+                   title: 'Yamaha Sniper 150',
+                   price: 99000,
+                   description: 'Gixxer is a comfortable bike with excellent manoeuvrability. The motorcycles large diameter front forks aid in reducing weight and contributes to sporty styling.',
+                   quantity: 0,
+               },
+               {
+                   id: 2,
+                   imageUrl: 'https://assetscdn1.paytm.com/images/catalog/product/S/SC/SCOSUZUKI-GIXXEJALA2721539BB31AF3/a_4.jpg',
+                   title: 'Suzuki Gixxer',
+                   price: 90000,
+                   description: 'The way the rear design is executed with th e body coloured grab rails almost merging into the panels that pack in a small triangular shaped LED taillight further adds to its looks.',
+                   quantity: 0,
+               },
+               {
+                   id: 3,
+                   imageUrl: 'https://news.webike.net/wp-content/uploads/2019/04/2019040902_NMAX155_04.jpg',
+                   title: 'Honda Xmax',
+                   price: 80000,
+                   description: 'High-tech features distance the XMAX from your typical bare-bones scooter, including LED lighting and advanced instrumentation. With plenty of lockable onboard storage.',
+                   quantity: 0,
+               },
+           ],
+
+           Headgears: [
+               {
+                   id: 4,
+                   imageUrl: 'https://www.revzilla.com/product_images/0356/8841/icon_airflite_helmet_black_750x750.jpg',
+                   title: 'Airflite Helmet',
+                   price: 1500,
+                   description: 'Its chin vent is functional to flow air and terrify a la a Hannibal Lecter face mask. Intake vents snort in so much fresh air that you can smell pedestrians fear as youre rolling down the block.',
+                   quantity: 0,
+               },
+               {
+                   id: 5,
+                   imageUrl: 'https://ph-live-01.slatic.net/original/c6aa7e0276f70e29f62170c89a40c8bf.jpg',
+                   title: 'RXR Nutshell Helmet Open Face V4',
+                   price: 550,
+                   description: 'Shock absorbing EPS inner liner for added protection, High-density Foam Fabric (soft and comfortable), Lightweight,',
+                   quantity: 0,
+               },
+           ],
+       }
   }
 
   addToCart = item => {
@@ -52,14 +75,22 @@ export default class App extends Component {
       <div className="App">
         <section className="products">
           <h1>Products</h1>
-          <h2>Hats</h2>
-          {this.state.hats.map(item => (
-            <Product key={item.id} item={item} addToCart={this.addToCart} />
+          <h2>Motorcycles</h2>
+          {this.state.Motorcycles.map(item => (
+            <Product 
+                key={item.id} 
+                item={item} 
+                addToCart={this.addToCart} 
+            />
           ))}
 
-          <h2>Beach Gear</h2>
-          {this.state.beachGear.map(item => (
-            <Product key={item.id} item={item} addToCart={this.addToCart} />
+          <h2>HeadGears</h2>
+          {this.state.Headgears.map(item => (
+            <Product 
+                key={item.id} 
+                item={item} 
+                addToCart={this.addToCart} 
+            />
           ))}
         </section>
 
@@ -72,17 +103,19 @@ export default class App extends Component {
               0
             )}
           </h2>
+          <div className="inputs">
+              <input type="text" placeholder="Address" value={this.setState.address} onChange={this.handleAddressInput} />
+
+              <input type="text" placeholder="Credit Card Number" value={this.setState.creditCard} onChange={this.handleCreditCardInput} />
+          </div>
           <button onClick={this.checkout}>Checkout</button>
+          
           {this.state.cart.map(item => (
-            <div key={item.id} className="product">
-              <img src={item.imageUrl} />
-              <div className="product-info">
-                <h4>{item.title}</h4>
-                <p>{item.description}</p>
-                <p>{item.price}</p>
-              </div>
-            </div>
-          ))}
+            <CartItem 
+                key={item.id}
+                item={item}
+            />
+          ))} 
         </section>
       </div>
     );
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-export default function Product(props) {
-  const { item, addToCart } = props;
+export default function CartItem(props) {
+  const { item } = props;
 
   return (
     <div class="product">
-      <img src={item.imageUrl} height="180" width="200" />
+      <img src={item.imageUrl} height="180" width="200"/>
       <div className="product-info">
         <h4>{item.title}</h4>
         <p>{item.price}</p>
         <p>{item.description}</p>
-        <button onClick={() => addToCart(item)}>Add to Cart</button>
       </div>
     </div>
   );
