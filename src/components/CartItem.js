f1500663a9b65f00922e960ac8b83c5ff95e9c4f
Sid <bercasio.sidney@gmail.com>
2019-06-24 12:40:34 +0800

    Done Part 2 Step 4

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Text from './Text';
 
 export default function CartItem(props) {
     const { item } = props;
@@ -8,9 +9,9 @@ export default function CartItem(props) {
       <div class="product">
         <img src={item.imageUrl} />
         <div className="product-info">
-          <h4>{item.title}</h4>
-          <p>{item.description}</p>
-          <p>{item.price}</p>
+        <Text isHeader={true} text={item.title} />
+        <Text isHeader={false} text={item.description} />
+        <Text isHeader={false} text={item.price} />
         </div>
       </div>
     );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Text from './Text';
 
 function Product(props){
     const { item, addToCart} = props;
@@ -8,9 +9,9 @@ function Product(props){
         <div key={item.id} className="product">
           <img src={item.imageUrl} />
           <div className="product-info">
-            <h4>{item.title}</h4>
-            <p>{item.description}</p>
-            <p>{item.price}</p>
+            <Text isHeader={true} text={item.title} />
+            <Text isHeader={false} text={item.description} />
+            <Text isHeader={false} text={item.price} />
             <button onClick={() => addToCart(item)}>Add to Cart</button>
           </div>
         </div>
