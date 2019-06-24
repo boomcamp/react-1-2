32a5cf161cbe93aa170c2aa06c016dc9c4b2e9dd
Sid <bercasio.sidney@gmail.com>
2019-06-24 11:45:43 +0800

    Done Step 1 & 2

---
+++ src/components/Product.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Product(props){
+    const { item, addToCart} = props;
+
+    return (
+        <div key={item.id} className="product">
+          <img src={item.imageUrl} />
+          <div className="product-info">
+            <h4>{item.title}</h4>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+            <button onClick={() => this.addToCart(item)}>Add to Cart</button>
+          </div>
+        </div>
+      );
+}
+
+export default Product;
